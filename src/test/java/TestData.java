19f6e8da28a09bc167c7a75c3f70f5d53be22f10
hadeelyn <61551822+hadeelyn@users.noreply.github.com>
2023-02-12 15:49:25 +0200

    hadeel

---
+++ src/test/java/TestData.java
@@ -0,0 +1,22 @@
+public class TestData {
+ public static String test="";
+    public class UserData{
+         static String name="Ibrahim";
+         static String mobile="98347589437";
+         static String academicDegree="Master";
+         static String specialization="Civil";
+         static int yearsOfExperince=10;
+         static String socailAccount="LinkedIn";
+    }
+
+    public class NewSpecialistData{
+         static String name="Ibrahim";
+         static String mobile="98347589437";
+         static String academicDegree="Master";
+         static String specialization="Civil";
+         static String yearsOfExperience="10";
+         static String socialAccount="LinkedIn";
+         static String successMessage="شكراً لك. لقد تم الإرسال بنجاح.";
+    }
+}
+
